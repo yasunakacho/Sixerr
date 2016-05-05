95c812d8e2f6ed3e5d68ba0fd9ca47d611a51e06
yasunakacho <yasu1987@me.com>
2016-05-05 14:52:27 +0900

    task10 finished

--- sixerrapp/views.py
+++ sixerrapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from .models import Gig
+from .models import Gig, Profile
 from .forms import GigForm
 
 # Create your views here.
@@ -46,7 +46,7 @@ def edit_gig(request, id):
                 error = "Data is not valid"
 
         return render(request, 'edit_gig.html', {"gig": gig, "error": error})
-        
+
     except Gig.DoesNotExist:
         return redirect('/')
 
@@ -54,3 +54,19 @@ def edit_gig(request, id):
 def my_gigs(request):
     gigs = Gig.objects.filter(user=request.user)
     return render(request, 'my_gigs.html', {"gigs": gigs})
+
+@login_required(login_url="/")
+def profile(request, username):
+    if request.method == 'POST':
+        profile = Profile.objects.get(user=request.user)
+        profile.about = request.POST['about']
+        profile.slogan = request.POST['slogan']
+        profile.save()
+    else:
+        try:
+            profile = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            return redirect('/')
+
+    gigs = Gig.objects.filter(user=profile.user, status=True)
+    return render(request, 'profile.html', {"profile": profile, "gigs": gigs})
