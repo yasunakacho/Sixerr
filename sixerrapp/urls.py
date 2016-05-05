3f270950edccb81210c71874e8d2dd0c8c83be55
yasunakacho <yasu1987@me.com>
2016-05-05 11:34:16 +0900

    task9 finished

--- sixerrapp/urls.py
+++ sixerrapp/urls.py
@@ -3,5 +3,8 @@ from sixerrapp import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_detail, name ='gig_detail')
+    url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_detail, name ='gig_detail'),
+    url(r'^my_gigs/$', views.my_gigs, name='my_gigs'),
+    url(r'^create_gig/$', views.create_gig, name='create_gig'),
+    url(r'^edit_gig/(?P<id>[0-9]+)/$', views.edit_gig, name='edit_gig'),
 ]
--- sixerrapp/views.py
+++ sixerrapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 from .models import Gig
+from .forms import GigForm
 
 # Create your views here.
 def home(request):
@@ -12,3 +15,42 @@ def gig_detail(request, id):
     except Gig.DoesNotExist:
         return redirect('/')
     return render(request, 'gig_detail.html', {"gig":gig})
+
+@login_required(login_url="/")
+def create_gig(request):
+    error = ''
+    if request.method == 'POST':
+        gig_form = GigForm(request.POST, request.FILES)
+        if gig_form.is_valid():
+            gig = gig_form.save(commit=False)
+            gig.user = request.user
+            gig.save()
+            return redirect('my_gigs')
+        else:
+            error = "Data is not valid"
+
+    gig_form = GigForm()
+    return render(request, 'create_gig.html', {"error": error})
+
+@login_required(login_url="/")
+def edit_gig(request, id):
+    try:
+        gig = Gig.objects.get(id=id, user=request.user)
+        error = ''
+        if request.method == 'POST':
+            gig_form = GigForm(request.POST, request.FILES, instance=gig)
+            if gig_form.is_valid():
+                gig.save()
+                return redirect('my_gigs')
+            else:
+                error = "Data is not valid"
+
+        return render(request, 'edit_gig.html', {"gig": gig, "error": error})
+        
+    except Gig.DoesNotExist:
+        return redirect('/')
+
+@login_required(login_url="/")
+def my_gigs(request):
+    gigs = Gig.objects.filter(user=request.user)
+    return render(request, 'my_gigs.html', {"gigs": gigs})
